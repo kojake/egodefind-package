1a8f882a22b8d0f2c8bad3fc8c476f750817633c
kaito <ahonamame@gmail.com>
2023-07-17 21:11:37 +0900

    update

--- initmainfile/main.py
+++ initmainfile/main.py
@@ -5,7 +5,11 @@ import json
 true = True
 false = False
 
+test_variable = None
+
 def true_variable(variable):
+    #エラー確認 
+    error_detection(variable)
     #jsonファイルに代入
     with open("json/true_false_list.json", "r", encoding="utf-8") as file:
         true_false_list = json.load(file)
@@ -14,6 +18,8 @@ def true_variable(variable):
         json.dump(true_false_list, file)
 
 def false_variable(variable):
+    #エラー確認 
+    error_detection(variable)
     #jsonファイルに代入
     with open("json/true_false_list.json", "r", encoding="utf-8") as file:
         true_false_list = json.load(file)
@@ -22,51 +28,73 @@ def false_variable(variable):
         json.dump(true_false_list, file)
 
 def see_variable(variable):
+    #エラー確認 
+    error_detection(variable)
     with open("json/true_false_list.json", "r", encoding="utf-8") as file:
         true_false_list = json.load(file)
     print(true_false_list[variable])
 
-#test
-def true_false_defined():
-    while True:
-        enter = input(">")
-        if enter == "true-variable":
-            print("変数名をつけてください")
-            variable = input(">")
-            true_variable(variable)
-            break
-        elif enter == "false-variable":
-            print("変数名をつけてください")
-            variable = input(">")
-            false_variable(variable)
-            break
-        elif enter == "see-variable":
-            print("どの変数のステータスを見ますか？")
-            variable = input(">")
-            see_variable(variable)
-            break
-        else:
-            print("そのようなコマンドはありません")
+def true_false_defined_use(variable):
+    #エラー確認 
+    error_detection(variable)
+    with open("json/true_false_list.json", "r", encoding="utf-8") as file:
+        true_false_list = json.load(file)
+    test_variable = true_false_list[variable]
+    print(test_variable)
 
-#test2
-while True:
-    enter = input(">")
-    if enter == "true-false-defined":
-        true_false_defined()
-    elif enter == "test_mode":
-        print("変数名を入力してください")
-        variable_enter = input(">")
-
-        with open("json/true_false_list.json", "r", encoding="utf-8") as file:
-            true_false_defined = json.load(file)
-
-        if variable_enter in true_false_defined.keys():
-            with open("json/true_false_list.json", "r", encoding="utf-8") as file:
-                true_false_list = json.load(file)
-            test_variable = true_false_list[variable_enter]
-            print(test_variable)
-        else:
-            print("一致する変数がありませんでした。")
+def error_detection(variable):
+    with open("json/true_false_list.json", "r", encoding="utf-8") as file:
+        true_false_list = json.load(file)
+    if variable in true_false_list.keys():
+        pass
     else:
-        print("そのようなコマンドはありません")
+        print("error: Variables " + variable + " is not difined")
+        exit()
+
+true_false_defined_use("true")
+true_false_defined_use("fewjfiewjofewi")
+
+#test
+# def true_false_defined():
+#     while True:
+#         enter = input(">")
+#         if enter == "true-variable":
+#             print("変数名をつけてください")
+#             variable = input(">")
+#             true_variable(variable)
+#             break
+#         elif enter == "false-variable":
+#             print("変数名をつけてください")
+#             variable = input(">")
+#             false_variable(variable)
+#             break
+#         elif enter == "see-variable":
+#             print("どの変数のステータスを見ますか？")
+#             variable = input(">")
+#             see_variable(variable)
+#             break
+#         else:
+#             print("そのようなコマンドはありません")
+
+# test2
+# while True:
+#     enter = input(">")
+#     if enter == "true-false-defined":
+#         true_false_defined()
+#     elif enter == "test_mode":
+#         print("変数名を入力してください")
+#         variable_enter = input(">")
+
+#         with open("json/true_false_list.json", "r", encoding="utf-8") as file:
+#             true_false_defined = json.load(file)
+
+#         if variable_enter in true_false_defined.keys():
+#             with open("json/true_false_list.json", "r", encoding="utf-8") as file:
+#                 true_false_list = json.load(file)
+#             test_variable = true_false_list[variable_enter]
+#             print(test_variable)
+#         else:
+#             print("一致する変数がありませんでした。")
+#     else:
+#         print("そのようなコマンドはありません")
 
