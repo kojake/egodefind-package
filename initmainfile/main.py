8b8905d55e321e32b4627591be4776a9e358e3b8
kaito <ahonamame@gmail.com>
2023-07-17 20:59:34 +0900

    update

--- initmainfile/main.py
+++ initmainfile/main.py
@@ -6,7 +6,6 @@ true = True
 false = False
 
 def true_variable(variable):
-    variable = True
     #jsonファイルに代入
     with open("json/true_false_list.json", "r", encoding="utf-8") as file:
         true_false_list = json.load(file)
@@ -15,7 +14,6 @@ def true_variable(variable):
         json.dump(true_false_list, file)
 
 def false_variable(variable):
-    variable = False
     #jsonファイルに代入
     with open("json/true_false_list.json", "r", encoding="utf-8") as file:
         true_false_list = json.load(file)
@@ -36,27 +34,39 @@ def true_false_defined():
             print("変数名をつけてください")
             variable = input(">")
             true_variable(variable)
+            break
         elif enter == "false-variable":
             print("変数名をつけてください")
             variable = input(">")
             false_variable(variable)
+            break
         elif enter == "see-variable":
             print("どの変数のステータスを見ますか？")
             variable = input(">")
             see_variable(variable)
+            break
         else:
             print("そのようなコマンドはありません")
 
 #test2
 while True:
     enter = input(">")
-    if enter == "true-variable":
-        true_variable()
+    if enter == "true-false-defined":
+        true_false_defined()
     elif enter == "test_mode":
+        print("変数名を入力してください")
+        variable_enter = input(">")
+
         with open("json/true_false_list.json", "r", encoding="utf-8") as file:
-            true_false_list = json.load(file)
-        test_variable = true_false_list[""]
-        print(test_variable)
+            true_false_defined = json.load(file)
+
+        if variable_enter in true_false_defined.keys():
+            with open("json/true_false_list.json", "r", encoding="utf-8") as file:
+                true_false_list = json.load(file)
+            test_variable = true_false_list[variable_enter]
+            print(test_variable)
+        else:
+            print("一致する変数がありませんでした。")
     else:
         print("そのようなコマンドはありません")
 
